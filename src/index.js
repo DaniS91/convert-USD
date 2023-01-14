8d189cc70abda2f453451e634ef9f5469daf276e
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-01-14 14:33:24 -0800

    add event listener to form and form submission handler to index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import ExchangeService from './exchangeservice.js';
 
 //Business Logic
 
-function getExchange() {
+function getExchange(currency, amount) {
   ExchangeService.getExchange()
     .then(function(response) {
       if (response.main) {
-        printElements(response);
+        printElements(response, currency, amount);
       } else {
         printError(response);
       }
@@ -27,3 +27,15 @@ function printError(error) {
   document.querySelector('#showResponse').innerText = `There was an error accessing the currency exchange rate: 
   ${error}.`;
 }
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const currency = document.querySelector('#currency').value;
+  const amount = document.querySelector('#amount').value;
+  // document.querySelector('#location').value = null;
+  getExchange(currency, amount);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
