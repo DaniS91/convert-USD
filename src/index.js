cc6c78facdff594b4579f54289106e3a5da13175
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-01-15 00:55:45 -0800

    add UI logic to handle input from form options and output correct data from api

--- src/index.js
+++ src/index.js
@@ -5,37 +5,40 @@ import ExchangeService from './exchangeservice.js';
 
 //Business Logic
 
-function getExchange(currency, amount) {
-  ExchangeService.getExchange()
-    .then(function(response) {
-      if (response.main) {
-        printElements(response, currency, amount);
-      } else {
-        printError(response);
-      }
-    });
+function getExchange() {
+  let promise = ExchangeService.getExchange();
+  promise.then(function(exchangeRate) {
+    printElements(exchangeRate);
+  }, function(error) {
+    printError(error);
+  });
 }
 
 //UI Logic
 
-function printElements(response, currency, amount) {
-  let newAmount = response.conversion_rate.EUR * amount
-  document.querySelector('#showResponse').innerText = `$${amount} USD is ${newAmount} ${currency}.`;
+function handleFormSubmission(event) {
+  event.preventDefault();
+  getExchange();
+}
+
+function printElements(data) {
+  const currencyCode = document.querySelector('#currency').value;
+  const amount = document.querySelector('#amount').value;
+  const exchangeArray = Object.values(data);
+  const currencyArray = Object.keys(data);
+  const currency = currencyArray[currencyCode];
+  const rate = exchangeArray[currencyCode];
+  const newAmount = rate * amount;
+  document.querySelector('#showResults').innerText = `$${amount} in ${currency} is ${newAmount}`;
 }
 
 function printError(error) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the currency exchange rate: 
-  ${error}.`;
-}
-
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const currency = document.querySelector('#currency').value;
-  const amount = document.querySelector('#amount').value;
-  // document.querySelector('#location').value = null;
-  getExchange(currency, amount);
+  const errorType = Object.values(error[1]);
+  console.log(errorType[3]);
+  document.querySelector('#showResults').innerText = `There was an error: ${errorType[3]}`;
 }
 
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
+
