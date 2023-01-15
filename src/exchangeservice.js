168efe494e725fb021fd4d5d1996357c4326c245
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-01-15 00:48:22 -0800

    correct syntax and remove comments

--- src/exchangeservice.js
+++ src/exchangeservice.js
@@ -1,21 +1,3 @@
-// export default class ExchangeService {  
-//   static getExchange() {
-//     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-//       .then(function(response) {
-//         if (!response.ok) {
-//           const errortype = 'error-type'
-//           const errorMessage = `${response.result}:  ${response[errortype]}`;
-//           throw new Error(errorMessage);
-//         } else {
-//           return response.json();
-//         }
-//       })      
-//       .catch(function(error) {
-//         return error;
-//       });
-//   }
-// }
-
 export default class ExchangeService {  
   static getExchange() {
     return new Promise(function(resolve, reject) {
