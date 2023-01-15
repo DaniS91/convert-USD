8ceef4c825f2344b18cfe9c0488ff0d5547e0bdc
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-01-14 16:40:34 -0800

    WIP: removed function and changed logic to use promise to handle api due to api issues

--- src/exchangeservice.js
+++ src/exchangeservice.js
@@ -1,16 +1,36 @@
+// export default class ExchangeService {  
+//   static getExchange() {
+//     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+//       .then(function(response) {
+//         if (!response.ok) {
+//           const errortype = 'error-type'
+//           const errorMessage = `${response.result}:  ${response[errortype]}`;
+//           throw new Error(errorMessage);
+//         } else {
+//           return response.json();
+//         }
+//       })      
+//       .catch(function(error) {
+//         return error;
+//       });
+//   }
+// }
+
 export default class ExchangeService {  
   static getExchange() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-      .then(function(response) {
-        if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}`;
-          throw new Error(errorMessage);
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response]);
         } else {
-          return response.json();
+          reject([this, response]);
         }
-      })      
-      .catch(function(error) {
-        return error;
       });
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
